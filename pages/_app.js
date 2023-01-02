69a46306ccdb43c29ac7ab9a0c54c7e32c90da93
hayashida-taihei <48717709+hayashida-taihei@users.noreply.github.com>
2023-01-02 12:45:07 +0900

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,6 @@
 import 'styles/globals.css'
 import Layout from 'components/layout'
 
-// Font Awesome の設定
-import '@fortawesome/fontawesome-svg-core/styles.css'
-import {config} from '@fortawesome/fontawesome-svg-core'
-config.autoAddCss =false
 
 function MyApp({ Component, pageProps }) {
   return (
